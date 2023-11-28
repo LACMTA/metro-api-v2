e96818f12edbf1ac4fe93198a973152e2c1df644
albertkun <albertk@gmx.com>
2023-11-28 07:53:55 -0800

    feat: [data] lock for gtfs-rt updates refactor: AWS deployment configurations

--- data-loading-service/app/main.py
+++ data-loading-service/app/main.py
@@ -9,14 +9,18 @@ import time
 import pandas as pd
 
 import crython
-# import schedule
-@crython.job(second='*/'+str(main_helper.set_interval_time())+'')
-def gtfs_rt_scheduler():
-    try:
-        gtfs_rt_helper.update_gtfs_realtime_data()
-    except Exception as e:
-        print('Error updating GTFS-RT data: ' + str(e))
 
+lock = threading.Lock()
+
+@crython.job(second='*/15')
+def gtfs_rt_scheduler():
+    with lock:
+        if lock.locked():
+            try:
+                gtfs_rt_helper.update_gtfs_realtime_data()
+            except Exception as e:
+                print('Error updating GTFS-RT data: ' + str(e))
+                
 @crython.job(expr='@daily')
 def go_pass_data_scheduler():
     try:
